314fdaa3e22d0ca947e857dbb8f7f9b99a8b168e
Beer Pratap Singh <beerpratap.s@turtlemint.com>
2020-03-11 14:53:46 +0530

    added active class code n minor css changes

--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
-let mainNavLinks = document.querySelectorAll(".nav-bar ul li a");
-let mainSections = document.querySelectorAll(".page-main section");
-
-let lastId;
-let cur = [];
-
-window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
-
-  mainNavLinks.forEach(link => {
-    let section = document.querySelector(link.hash);
-
-    if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
-      link.classList.add("current");
-    } else {
-      link.classList.remove("current");
-    }
+$(document).ready( () => {
+  $('.nav-bar').find("a").click(function (e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $('.nav-bar a').removeClass('active');
+    $(this).addClass('active')
+    $("html, body").animate({
+      scrollTop: $(section).offset().top
+    });
   });
 });
 
 
-$(document).ready( () => {
-  $('#submitBtn').on('click', (e) => {
-    e.preventDefault();
-    console.log('====================================');
-    console.log('Clicked');
-    console.log('====================================');
-  })
+const Http = new XMLHttpRequest();
+const url = 'https://formspree.io/biru.harshi@gmail.com';
+Http.open("POST", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
+
+$('#submitBtn').on('click', (e) => {
+  // e.preventDefault();
 })
